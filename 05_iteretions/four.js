405dd1729598959046d52adf528f3a376f5bef55
bsalma2303 <151726337+bsalma2303@users.noreply.github.com>
2023-11-22 10:16:33 +0000

    for in four

---
+++ 05_iteretions/four.js
@@ -0,0 +1,10 @@
+const myObject={
+    js: 'javascript',
+    cpp:'c++',
+    rb: 'rubby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    //console.log(key);  // jd cpp rb swift
+}
